05ca7bb967b11cbce6560f46dfe760bbbbbe6296
15713028819 <1207994117@qq.com>
2021-07-20 17:43:05 +0800

    Create package.js

---
+++ build/package.js
@@ -0,0 +1,12 @@
+{
+  "name": "test",
+  "version": "1.0.0",
+  "description": "{{description}}",
+  "private": true,
+  "dependencies": {
+    "lodash": "*"
+  },
+  "scripts": {
+    "build": "true"
+  }
+}
